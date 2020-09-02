d5b7b970b51f30fdfc2009eb3522e790fdb4f7bc
Andre Furquim <=>
2020-09-02 13:22:23 -0300

    :wrench: Initial ESLint and Prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: ['react'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    node: true,
+  },
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+  settings: {
+    react: {
+      version: '16.3.1',
+    },
+  },
+};
